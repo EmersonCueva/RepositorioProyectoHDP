047ea923bcd0267c7ba7b4d3967b93bbc40da2cc
EmersonCueva <92651645+EmersonCueva@users.noreply.github.com>
2023-06-17 22:52:31 -0600

    Version 2 del proyecto
    Implementa los cuatro elementos del crud, es decir que nos permite buscar,eliminar,modificar y agregar a un empleado con sus datos, ademas de agregar algunas vistas

---
+++ carpeta/capaEntidad/direccionEmpleado.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaEntidad
+{
+//    CREATE TABLE direccionEmpleado(
+//    idDireccion INT PRIMARY KEY,
+//    sucursalEmpleado VARCHAR(100),
+//    paisEmpleado VARCHAR(100),
+//    estado VARCHAR(100),
+//    nombreCalle VARCHAR(100),
+//    coloniaEmpleado VARCHAR(100),
+//    codigoPostal INT,
+//    activoDireccion BIT DEFAULT 1
+//);
+
+    public class direccionEmpleado
+    {
+        public int idDireccion { get; set; }
+        public string sucursalEmpleado { get; set; }
+        public string paisEmpleado { get; set; }
+        public string estado { get; set; }
+        public string nombreCalle { get; set; }
+        public string coloniaEmpleado { get; set; }
+        public int codigoPostal { get; set; }
+        public bool activoDireccion { get; set; }
+    }
+}
